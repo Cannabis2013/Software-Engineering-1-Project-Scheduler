3a816b119c7585de197fbc2121431c79579bfc9e
MH2012 <havnetrold2002@yahoo.dk>
2019-05-02 23:50:04 +0200

    Added "editRegistrationHour" test scenario, JUnit.

--- ProjectScheduler/Unit tests/allTests/HourRegistrationTests.java
+++ ProjectScheduler/Unit tests/allTests/HourRegistrationTests.java
@@ -1,6 +1,5 @@
 package allTests;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.Arrays;
@@ -8,7 +7,6 @@ import java.util.List;
 import org.junit.Test;
 
 import baseClass.TestTemplate;
-import formComponents.ItemModel;
 import models.ProjectModel;
 
 public class HourRegistrationTests extends TestTemplate {
@@ -65,70 +63,15 @@ public class HourRegistrationTests extends TestTemplate {
 	}
 	
 	@Test
-	public void registerHourUnSuccesfully()
-	{
-		int workHours = 4;
-		boolean result = addRegistrationObject("FL", projectName, activityName, "Menus", workHours, "");
-		
-		assertEquals(false, result);
-	}
-	
-	@Test
-	public void removeActivitySuccesfully()
-	{
-		try {
-			coreApp.login("FL");
-		} catch (Exception e1) {
-			fail();
-		}
-		try {
-			coreApp.removeActivity(projectName, activityName);
-		} catch (Exception e) {
-			fail();
-		}
-	}
-	@Test
-	public void removeProject()
-	{
-		try {
-			coreApp.login("admin");
-		} catch (Exception e) {
-			fail();
-		}
-		try {
-			coreApp.removeProject(projectName);
-		} catch (Exception e) {
-			fail();
-		}
-	}
-	@Test
-	public void retrieveActivityItemModels()
-	{
-		try {
-			coreApp.login("TT");
-		} catch (Exception e) {
-			fail();
-		}
-		ItemModel[] itemModels = coreApp.activityItemModels();
-		
-		assertTrue(itemModels.length == 1);
-	}
-	@Test
-	public void retrieveProjectItemModels()
-	{
-		try {
-			coreApp.login("admin");
-		} catch (Exception e) {
-			fail();
-		}
-		ItemModel[] itemModels = coreApp.projectItemModels();
-		assertTrue(itemModels.length == 1);
-	}
-	
-	@Test
 	public void editHourRegistration()
 	{
+		String regId = "Menus";
+		int workHours = 8;
+		if(!addRegistrationObject("BB",projectName, activityName, regId, workHours, ""))
+			fail();
 		
+		if(!editRegistrationObject("BB", projectName, activityName, regId, 4))
+			fail();
 	}
 	
 }
