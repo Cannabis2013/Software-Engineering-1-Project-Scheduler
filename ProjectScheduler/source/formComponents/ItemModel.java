dc524f51b3c0139363c54c2c4bea8a702cb4b55d
MH2012 <havnetrold2002@yahoo.dk>
2019-05-07 21:05:24 +0200

    Updated ItemModel

--- ProjectScheduler/source/formComponents/ItemModel.java
+++ ProjectScheduler/source/formComponents/ItemModel.java
@@ -2,73 +2,56 @@ package formComponents;
 
 public class ItemModel {
 	
-	private String val;
-	private String[] subVals = new String[0];
+	private String[] itemData = new String[1];
 	
 	public ItemModel(String[] columnValues)
 	{
-		val = columnValues[0];
-		subVals = new String[columnValues.length - 1];
-		for (int i = 1; i < columnValues.length; i++)
-			subVals[i - 1] = columnValues[i];
+		itemData = columnValues;
 		
 	}
 	public ItemModel(String text)
 	{
-		val = text;
+		itemData[0] = text;
 	}
 	
 	public String text(int col)
 	{
-		if(col < 0)
-			return null;
-		if(col == 0)
-			return val;
-		if(col > subVals.length)
-			return null;
-		else
-			return subVals[col - 1];
+		return itemData[col];
 	}
 	
 	public void setText(String text, int column)
 	{
-		if(column < 0)
-			return;
-		if(column == 0)
-			val = text;
-		if(column > subVals.length -1)
-			return;
-		else
-			subVals[column] = text;
+		String[] tempData = itemData;
+		if(itemData.length <= column)
+			itemData = new String[column + 1];
+		
+		for (int i = 0; i < itemData.length; i++) {
+			if(i == column)
+				itemData[i] = text;
+			else if(i < tempData.length)
+				itemData[i] = tempData[i];
+			else
+				itemData[i] = "";
+		}
 	}
 	
 	public int columnCount()
 	{
-		return subVals.length + 1;
+		return itemData.length + 1;
 	}
 	
 	public boolean equals(ItemModel compare)
 	{
-		if(compare.text(0) != val)
-			return false;
-		
-		for (int i = 0; i < subVals.length; i++) {
-			if(!subVals[i].equals(compare.subVals[i]))
+		for (int i = 0; i < itemData.length; i++) {
+			if(!itemData[i].equals(compare.itemData[i]))
 				return false;
 		}
-		
 		return true;
 	}
 	
 	public String[] data()
 	{
-		String[] d = new String[columnCount()];
-		d[0] = val;
-		for (int i = 1; i < columnCount(); i++) {
-			d[i] = subVals[i - 1];
-		}
-		
-		return d;
+		return itemData;
 	}
 	
 }
